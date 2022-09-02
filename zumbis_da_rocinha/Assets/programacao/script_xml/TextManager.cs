54432c6a3bb1736521a583b504965a24f57892c0
Ast <vitor.orsin@estudante.ufscar.br>
2022-09-02 15:21:32 -0300

    Atualização do script e base para a UI de escolha

--- zumbis_da_rocinha/Assets/programacao/script_xml/TextManager.cs
+++ zumbis_da_rocinha/Assets/programacao/script_xml/TextManager.cs
@@ -4,50 +4,123 @@ using UnityEngine;
 using System.IO;
 using System.Xml;
 using UnityEngine.UI;
+using TMPro;
 
 public class TextManager : MonoBehaviour
 {
+    [SerializeField] private XML_reading xReader;   // Instanciação da classe que lê o XML
+    public string blocoS = "0";
+
+    public GameObject canvas;                       // Parente de todas as UI bases
+
+    public GameObject prefabDialogo;                // Prefab da UI de dialogo
     private Queue<XmlNode> falas;
 
-    public string bloco = "0";
-    [SerializeField] private XML_reading xReader;
-    [SerializeField] private Text CaixaNome;
-    [SerializeField] private Text CaixaTexto;
+    public GameObject prefabEscolhas;               // Prefab da UI de escolhas
+    public GameObject prefabBotao;                  // Prefab dos botões da UI de escolha
 
+    private Vector3 pos1;
+    private Vector3 pos2;
+    
     void Start(){
         falas = new Queue<XmlNode>();
-        LoadDialogue(bloco);
+        LoadDialogue("0"); // Chama a primeira execução de bloco
     }
 
     public void LoadDialogue(string bloco){
         xReader.LoadFile();
-        XmlNodeList dialogos = xReader.ParseDialogo(bloco);
-        foreach(XmlNode dialogo in dialogos){
-            falas.Enqueue(dialogo);
+        foreach(XmlNode dialogo in xReader.ParseDialogo(bloco)){
+            falas.Enqueue(dialogo); // Adiciona todos os nodes na fila
         }
+
+        blocoS = bloco;
+        Destroy(GameObject.Find("escolhasUI(Clone)"));  // Limpa a tela se necessário
+        Instantiate(prefabDialogo, canvas.transform);   // Instancia a UI de diálogo
+
+        //Atualiza o trigger do "botao" que chama a proxima parte do dialogo 
+        GameObject trigger = GameObject.Find("dialogoUI(Clone)/Trigger");
+        trigger.GetComponent<Button>().onClick.AddListener(CallNextDialogue);
 
         CallNextDialogue();
     }
 
     public void CallNextDialogue(){
-        if(falas.Count == 0) return;// Inserir chamada de escolhas aqui depois
-
-        Debug.Log(falas.Count.ToString());
+        // i.e. quando acabar o diálogo
+        if(falas.Count == 0){
+            Destroy(GameObject.Find("dialogoUI(Clone)"));   // Limpa a tela
+            Instantiate(prefabEscolhas, canvas.transform);  // Instancia a UI de escolha
+            CallEscolhas();
+            return;
+        }
+        // Puxa um node da fila para ser utilizado
         XmlNode fala = falas.Dequeue();
-
         XmlNode nome = fala.FirstChild;
         XmlNode texto = nome.NextSibling;
 
-        CaixaNome.text = nome.InnerXml;
+        GameObject caixaNome = GameObject.Find("dialogoUI(Clone)/CaixaNomeFrame");
+        pos1 = caixaNome.transform.localPosition;
+        pos2 = caixaNome.transform.localPosition - new Vector3(2*caixaNome.transform.localPosition.x,0,0);
+
+        // Atualiza a caixa de nome da personagem
+        // Cool dynamic with position wow
+        if(caixaNome.GetComponentInChildren<TextMeshProUGUI>().text != nome.InnerXml){
+            if(nome.InnerXml == "..." || nome.InnerXml == "Protagonista")
+                caixaNome.transform.localPosition = pos1;
+            else if(caixaNome.transform.localPosition == pos1 && caixaNome.GetComponentInChildren<TextMeshProUGUI>().text !="")
+                caixaNome.transform.localPosition = pos2;
+            else if(caixaNome.transform.localPosition == pos2)
+                caixaNome.transform.localPosition = pos1;
+            caixaNome.GetComponentInChildren<TextMeshProUGUI>().text = nome.InnerXml;
+        }
+
+        // Atualiza a caixa de texto
+        // Cool Dynamic with gradual texting wow
         StopAllCoroutines();
         StartCoroutine(LBLTyping(texto));
     }
 
+    // sub rotina para imprimir o texto letra por letra
     IEnumerator LBLTyping(XmlNode texto){
-        CaixaTexto.text = "";
+        GameObject caixaTexto = GameObject.Find("dialogoUI(Clone)/CaixaDialogoFrame/CaixaDialogo");
+        caixaTexto.GetComponent<TextMeshProUGUI>().text = "";
         foreach(char letra in texto.InnerXml.ToCharArray()){
-            CaixaTexto.text += letra;
+            caixaTexto.GetComponent<TextMeshProUGUI>().text += letra;
             yield return null;
+        }
+    }
+
+    public void CallEscolhas(){
+        int i = 1;
+
+        // Atualiza a caixa de resumo da escolha
+        GameObject resumoEscolha = GameObject.Find("escolhasUI(Clone)/CaixaEscolhasFrame/ResumoEscolha");
+        resumoEscolha.GetComponentInChildren<TextMeshProUGUI>().text = xReader.ParseResumo(blocoS);
+
+        foreach(XmlNode escolha in xReader.ParseEscolhas(blocoS)){
+            // Cria um botao pra cada node da NodeList
+            GameObject botao = Instantiate(prefabBotao, GameObject.Find("escolhasUI(Clone)/CaixaEscolhasFrame").transform);
+            // Atualiza a posição da instância na UI
+            switch (i)
+            {
+                case 2:
+                    botao.transform.localPosition += new Vector3(330,0,0);
+                    break;
+                case 3:
+                    botao.transform.localPosition += new Vector3(0,-35,0);
+                    break;
+                case 4:
+                    botao.transform.localPosition += new Vector3(330,-35,0);
+                    break;
+            }
+
+            // Atualiza o texto do botao
+            botao.GetComponentInChildren<TextMeshProUGUI>().text = escolha.InnerXml;
+
+            // Atualiza o trigger do botao **
+            string proximoBloco = (int.Parse(blocoS) + i).ToString();
+            botao.GetComponent<Button>().onClick.AddListener(delegate{LoadDialogue(proximoBloco);});
+            
+            i++;
         }
     }
 }
--- zumbis_da_rocinha/Assets/programacao/script_xml/XML_reading.cs
+++ zumbis_da_rocinha/Assets/programacao/script_xml/XML_reading.cs
@@ -5,20 +5,36 @@ using System.IO;
 using System.Xml;
 using UnityEngine.UI;
 
+// Classe não é MonoBehaviour, então funciona como um classe básica de C#
 [System.Serializable]
 public class XML_reading {
-    [SerializeField] TextAsset xmlFile;
+    [SerializeField] private TextAsset xmlFile; // Arquivo XML a ser lido
     public XmlDocument xmlDoc;
+    private string xPath;
 
+    // Carrega o arquivo em uma string e le o documento a partir dela
     public void LoadFile(){ 
         string data = xmlFile.text;
         xmlDoc = new XmlDocument();
         xmlDoc.Load(new StringReader(data));
     }
 
+    // Retorna uma lista com todos os nodes de dialogo de determinado bloco
     public XmlNodeList ParseDialogo(string bloco){
-        string xmlPath = "//cena1/bloco[@s='"+ bloco +"']/dialogos/dialogo";
-        XmlNodeList dialogos = xmlDoc.SelectNodes(xmlPath);
-        return dialogos;
+        xPath = "//cena/bloco[@s='"+ bloco +"']/dialogos/dialogo";
+        return xmlDoc.SelectNodes(xPath);;
     }
+
+    // Retorna o texto do node resumo (textinho da UI de escolha)
+    public string ParseResumo(string bloco){
+        xPath = "//cena/bloco[@s='"+ bloco +"']/escolhas/resumo";
+        return xmlDoc.SelectNodes(xPath)[0].InnerXml;
+    }
+
+    // Retorna uma lista com os nodes de escolha de determinado bloco
+    public XmlNodeList ParseEscolhas(string bloco){
+        xPath = "//cena/bloco[@s='"+ bloco +"']/escolhas/escolha";
+        return xmlDoc.SelectNodes(xPath);
+    }
+
 }
